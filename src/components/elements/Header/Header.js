cfd6fd0a75875674373846340320a29d87137c09
Matan Shaviro <matan3sh@matans-mbp.mynet>
2019-01-10 22:41:42 +0200

    upload the website to github

---
+++ src/components/elements/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="rmdb-header">
+      <div className="rmdb-header-content">
+        <Link to="/">
+          <img className="rmdb-logo" src="./images/reactMovie_logo.png" alt="rmdb-logo" />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Header;
